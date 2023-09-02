a2590d2a220514cc6a3586b1d14f0e51e139c556
Dat Proto <contact@datproto.com>
2023-09-03 03:52:21 +0700

    FINISH: Navbar (static)

---
+++ components/BackgroundOverlay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BackgroundOverlay = () => {
+  return (
+    <div className='absolute top-0 right-0 w-full h-full bg-black opacity-75 z-40' />
+  )
+}
+
+export default BackgroundOverlay
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'theme': {
+          'black': '#1D2026',
+          'orange': '#FF7E1B',
+          'gray': {
+            'normal': '#69707D',
+            'lighter': '#B6BCC8'
+          },
+          'smoke': {
+            'normal': '#E4E9F2',
+            'lighter': '#F6F8FD'
+          },
+          'beige': '#FFEEE2'
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
