385d0322951232df8eb24b266a0b256d7ff8cea8
Dat Proto <contact@datproto.com>
2023-09-03 03:59:43 +0700

    UPDATE: Add eslint rules for tailwindcss

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,14 +18,16 @@ const Navbar = ({isOpen, triggerMenuClose}: INavbar) => {
    */
 
   return (
-    <nav className={`absolute lg:relative h-full w-2/3 transform bg-white p-6 flex flex-col lg:flex-row gap-10 z-50 top-0 ${isOpen ? 'left-0' : 'left-[-70%]'} lg:left-0 transition-all`}>
-      <button type='button' onClick={triggerMenuClose}>
-        <Image src='/icons/icon-close.svg' alt='Close Button' width={14} height={15} />
+    <nav
+      className={`absolute top-0 z-50 flex h-full w-2/3 transform flex-col gap-10 bg-white p-6 lg:relative lg:flex-row ${isOpen ? 'left-0' : 'left-[-70%]'} transition-all lg:left-0`}>
+      <button type="button" onClick={triggerMenuClose}>
+        <Image src="/icons/icon-close.svg" alt="Close Button" width={14} height={15}/>
       </button>
-      <ul className="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-8">
+      <ul className="flex flex-col gap-4 lg:flex-row lg:items-center lg:gap-8">
         {navLinks.map(link => (
           <li key={link.key}>
-            <Link href={link.href} className="text-theme-black lg:text-theme-gray-normal font-bold lg:font-normal text-[1.125rem] lg:text-[0.9375rem]">
+            <Link href={link.href}
+                  className="text-[1.125rem] font-bold text-theme-black lg:text-[0.9375rem] lg:font-normal lg:text-theme-gray-normal">
               {link.text}
             </Link>
           </li>
@@ -41,19 +43,20 @@ const Header = () => {
   }
 
   return (
-    <header className='bg-white p-6 flex items-center justify-between border-b border-b-theme-smoke-normal'>
-      {isMobileMenuOpen && <BackgroundOverlay />}
+    <header className="flex items-center justify-between border-b border-b-theme-smoke-normal bg-white p-6">
+      {isMobileMenuOpen && <BackgroundOverlay/>}
 
-      <div id="left-menu" className='flex gap-4 lg:gap-0 items-center'>
-        <button type='button' onClick={triggerMobileMenu} className='lg:hidden'>
-          <Image src='/icons/icon-menu.svg' alt='Menu Icon' width={16} height={15} />
+      <div id="left-menu" className="flex items-center gap-4 lg:gap-0">
+        <button type="button" onClick={triggerMobileMenu} className="lg:hidden">
+          <Image src="/icons/icon-menu.svg" alt="Menu Icon" width={16} height={15}/>
         </button>
-        <Image src='/images/logo.svg' alt='Logo' width={132} height={20} />
-        <Navbar isOpen={isMobileMenuOpen} triggerMenuClose={triggerMobileMenu} />
+        <Image src="/images/logo.svg" alt="Logo" width={132} height={20}/>
+        <Navbar isOpen={isMobileMenuOpen} triggerMenuClose={triggerMobileMenu}/>
       </div>
-      <div id="right-menu" className='flex gap-4 lg:gap-8 items-center'>
-        <Image src='/icons/icon-cart.svg' alt='icon-cart' width={22} height={20} />
-        <Image src='/images/image-avatar.png' alt='avatar' width={100} height={100} className='object-cover h-6 w-6 lg:h-[3.125rem] lg:w-[3.125rem]' />
+      <div id="right-menu" className="flex items-center gap-4 lg:gap-8">
+        <Image src="/icons/icon-cart.svg" alt="icon-cart" width={22} height={20}/>
+        <Image src="/images/image-avatar.png" alt="avatar" width={100} height={100}
+               className="h-6 w-6 object-cover lg:h-[3.125rem] lg:w-[3.125rem]"/>
       </div>
     </header>
   )
